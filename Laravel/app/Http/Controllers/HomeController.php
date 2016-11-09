e0854475d9e967429cdd5ac080ef8391c896f32c
Adriano Barra <klein_adi@hotmail.com>
2016-11-09 15:32:11 +0100

    HomeController gfixt

--- Laravel/app/Http/Controllers/HomeController.php
+++ Laravel/app/Http/Controllers/HomeController.php
@@ -2,12 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+
 
 class HomeController extends Controller
 {
     /**
-     * Create a new controller instanc
+     * Create a new controller instance.
+     *
      * @return void
      */
     public function __construct()
