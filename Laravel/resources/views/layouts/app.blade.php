62cbdea7ebac3b37832c3090ea7f20edaea44888
Adriano Barra <klein_adi@hotmail.com>
2016-11-10 14:44:48 +0100

    Titel geändert

--- Laravel/resources/views/layouts/app.blade.php
+++ Laravel/resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>Carousel Template for Bootstrap</title>
+    <title>MyClub</title>
 
     <!-- Scripts -->
     <script>
